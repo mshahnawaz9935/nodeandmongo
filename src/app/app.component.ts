13aa514f840844e430a9c1609e49c2449ac2219c
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-04-10 16:49:19 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,16 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'app works!';
-  data;
+  data = [];
+  text = '';
   constructor(private http:Http){
     console.log('how is it going');
     this.http.get('http://localhost:3000/getdata')
             .map((res: Response) => res.json()).subscribe((dataFromServer) => {
       // Now you can use the data
       this.data = dataFromServer;
-      console.log(this.data);
+      this.text  = this.data[1].name;
+      console.log('my data is' + this.data[0],  this.data[1].name );
     });
 
   
