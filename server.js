72f3c6576e12d58db4a61922f4daab3143a76c23
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-04-10 16:14:51 +0100

    changes

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+//const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//app.use('/api', api);
+
+app.get('/getdata', (req, res) => {
+  res.json('api works');
+});
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http  ,Response, Headers, RequestOptions} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  data;
+  constructor(private http:Http){
+    console.log('how is it going');
+    this.http.get('http://localhost:3000/getdata')
+            .map((res: Response) => res.json()).subscribe((dataFromServer) => {
+      // Now you can use the data
+      this.data = dataFromServer;
+      console.log(this.data);
+    });
+
+  
+  }
+  
 }
