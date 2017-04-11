28036dfff93655998191e0321a626f599a3cc1c7
mshahnawaz9935 <mshahnawaz9935@gmail.com>
2017-04-11 18:37:23 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Http  ,Response, Headers, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
